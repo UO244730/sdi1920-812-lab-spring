9773b62d9ec30b01be2e73e165da54af5045fa11
UO244730 <UO244730@uniovi.es>
2020-02-16 17:19:38 +0100

    SDI-SpringBoot-4-Acceso a datos con entidades relacionadas

--- src/main/java/com/uniovi/entities/Mark.java
+++ src/main/java/com/uniovi/entities/Mark.java
@@ -3,6 +3,8 @@ package com.uniovi.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 @Entity
 public class Mark {
 	@Id
@@ -11,12 +13,22 @@ public class Mark {
 	private String description;
 	private Double score;
 
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 	
 	public Mark(Long id, String description, Double score) {
 		super();
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+	
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
 	}
 
 	public Mark() {
