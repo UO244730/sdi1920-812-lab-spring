e44cc30abae5861af2e4362077cbd73f0bd8c53a
UO244730 <UO244730@uniovi.es>
2020-02-23 14:59:09 +0100

    SDI-SpringBoot-Sesion-4.3-Control de acceso por roles

--- src/main/java/com/uniovi/controllers/MarksControllers.java
+++ src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -16,7 +16,7 @@ import com.uniovi.services.UsersService;;
 
 @Controller
 public class MarksControllers {
-	
+
 	@Autowired
 	private HttpSession httpSession;
 
@@ -28,8 +28,7 @@ public class MarksControllers {
 
 	@RequestMapping("/mark/list")
 	public String getList(Model model) {
-		
-		
+
 		model.addAttribute("markList", marksService.getMarks());
 		return "mark/list";
 
@@ -81,5 +80,17 @@ public class MarksControllers {
 	public String updateList(Model model) {
 		model.addAttribute("markList", marksService.getMarks());
 		return "mark/list :: tableMarks";
+	}
+
+	@RequestMapping(value = "/mark/{id}/resend", method = RequestMethod.GET)
+	public String setResendTrue(Model model, @PathVariable Long id) {
+		marksService.setMarkResend(true, id);
+		return "redirect:/mark/list";
+	}
+
+	@RequestMapping(value = "/mark/{id}/noresend", method = RequestMethod.GET)
+	public String setResendFalse(Model model, @PathVariable Long id) {
+		marksService.setMarkResend(false, id);
+		return "redirect:/mark/list";
 	}
 }
--- src/main/java/com/uniovi/entities/Mark.java
+++ src/main/java/com/uniovi/entities/Mark.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
 @Entity
 public class Mark {
 	@Id
@@ -12,18 +13,19 @@ public class Mark {
 	private Long id;
 	private String description;
 	private Double score;
+	private Boolean resend = false;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
-	
+
 	public Mark(Long id, String description, Double score) {
 		super();
 		this.id = id;
 		this.description = description;
 		this.score = score;
 	}
-	
+
 	public Mark(String description, Double score, User user) {
 		super();
 		this.description = description;
@@ -63,4 +65,22 @@ public class Mark {
 	public String toString() {
 		return "Mark [id=" + id + ", descrption=" + description + ", score=" + score + "]";
 	}
+
+	public Boolean getResend() {
+		return resend;
+	}
+
+	public void setResend(Boolean resend) {
+		this.resend = resend;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	
 }
